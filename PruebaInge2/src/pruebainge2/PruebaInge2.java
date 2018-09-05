a184c0daa6816614f84b411266210e2e9346262f
Anthony Ramirez Lopez <32112160+Tony1040@users.noreply.github.com>
2018-09-05 13:34:14 -0600

    Prueba de commit

--- PruebaInge2/src/pruebainge2/PruebaInge2.java
+++ PruebaInge2/src/pruebainge2/PruebaInge2.java
@@ -15,7 +15,9 @@ public class PruebaInge2 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        System.out.println("Esto es una prueba para ver si sirve esta vara :'v");
+        System.out.println("x2");
+
     }
-    
+
 }
